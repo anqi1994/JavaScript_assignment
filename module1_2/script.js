fff4ca4362ebd8efc5f2f4c6fa95932b63f14858
anqi1994 <142995905+anqi1994@users.noreply.github.com>
2023-11-20 13:40:59 +0200

    Delete module1_2 directory

--- module1_2/script.js
+++
@@ -1,6 +0,0 @@
-const name = prompt("What is your name?")
-
-function display() {
-    const message = "Hello " + name + " !";
-    document.getElementById("output").innerHTML = "<p>" + message + "</p>";
-}
